f223d58dede0d3ba24bbab14adaa1732b93b0b6c
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-09-02 20:27:31 -0300

    CRUD em Product

---
+++ node-api/src/controllers/ProductController.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Product = mongoose.model('Product');
+
+module.exports = {
+
+   //VIEW
+   async index(req, res) {
+      const products = await Product.find();
+      return res.json(products);
+   },
+
+   //DETAIL
+   async show(req, res) {
+      const product = await Product.findById(req.params.id);
+      return res.json(product);
+   },
+
+   //CREATE
+   async create(req, res) {
+      const product = await Product.create(req.body);
+      return res.json(product);
+   },
+
+   //UPDATE
+   async update(req, res) {
+      const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true});
+      return res.json(product);
+   },
+
+   //DELETE
+   async destroy(req, res) {
+      const product = await Product.findByIdAndDelete(req.params.id);
+      return res.send(product.title + ' deletado com sucesso!')
+   }
+};
